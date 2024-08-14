b0b633bdaebbe698229f668e2e7d24ef377b08d3
sivchari <shibuuuu5@gmail.com>
2024-08-14 16:53:53 +0900

    implement Run command
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -2,15 +2,33 @@ package run
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/goccy/go-yaml"
 	"github.com/spf13/pflag"
+
+	"github.com/sivchari/gomod"
 )
 
 type Command struct{}
 
 func (c *Command) Run(ctx context.Context) error {
-	fmt.Println("Run")
+	if file == nil || *file == "" {
+		panic(errors.New("file is required"))
+	}
+	f, err := os.ReadFile(*file)
+	if err != nil {
+		panic(fmt.Errorf("RunCommand read file, but got error: %w", err))
+	}
+	cfg := &gomod.GoModConfig{}
+	if err := yaml.Unmarshal(f, cfg); err != nil {
+		panic(fmt.Errorf("RunCommand unmarshal yaml, but got error: %w", err))
+	}
+	if err := gomod.Replace(ctx, cfg); err != nil {
+		panic(fmt.Errorf("RunCommand tried to replace go version, but got error: %w", err))
+	}
 	return nil
 }
 
@@ -26,6 +44,8 @@ func (c *Command) Long() string {
 	return "Run command"
 }
 
+var file *string
+
 func (c *Command) SetFlags(f *pflag.FlagSet) {
-	f.String("name", "run", "Name")
+	f.StringVarP(file, "file", "f", "", "File path")
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,17 @@ module github.com/sivchari/gomod
 go 1.23
 
 require (
+	github.com/goccy/go-yaml v1.12.0
 	github.com/google/go-cmp v0.6.0
 	github.com/sivchari/commander v0.1.0
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/mod v0.20.0
+)
+
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
